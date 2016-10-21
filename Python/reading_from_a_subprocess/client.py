9513eaacb8e63187f4897726afe175a2af5d5d3d
Dima Rudaev <RudaevDima@gmail.com>
2016-10-21 11:25:29 +0300

    python: few reading from subprocess examples added

---
+++ Python/reading_from_a_subprocess/client.py
@@ -0,0 +1,17 @@
+from subprocess import Popen, PIPE
+from time import sleep
+
+# run the shell as a subprocess:
+p = Popen(['python', 'shell.py'],
+          stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False)
+# issue command:
+p.stdin.write('command\n')
+# let the shell output the result:
+sleep(0.1)
+# get the output
+while True:
+    output = p.stdout.read() # <-- Hangs here!
+    if not output:
+        print '[No more data]'
+        break
+    print output
